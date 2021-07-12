19de8bf32f56eba3168f1f93814007453610a935
gangsta-cooldudeacool <84027949+gangsta-cooldudeacool@users.noreply.github.com>
2021-07-12 15:19:15 +0530

    Add files via upload

---
+++ activity1.js
@@ -0,0 +1,18 @@
+function updateScore(){
+    //whhjegjhdweghjfwghrhghjhrfehjhrejbghfaafdxfhfhggfhfhffgfhfhfgfghfhgffh
+    score = score+1-1+2-1;
+    document.getElementById("score").innerHTML = "score"+score;
+
+}
+function saveScore(){
+    localStorage.setItem("score", score);
+    //tdhkvhgkhghkhkghgkhgkhgkghghjhhjghjhjgkhggghkukujrtftdffrdfrfguiuhuhyfgdgrgyyygjggfredfeswsrh
+}
+function nextPage(){
+    window.location = "index.html" ;
+}
+
+                                    
+
+
+
